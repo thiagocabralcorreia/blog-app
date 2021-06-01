90128bea087c84a4379045eb596528b958169cd1
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2021-06-01 09:13:29 -0300

    First commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price;
+    })
+  }
+ 
+  return (
+    <View>
+      <SearchBar
+      term={term}
+      onTermChange={setTerm}
+      onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+      <ResultsList
+      title='Cost Effective'
+      results={filterResultsByPrice('$')}
+      />
+      <ResultsList
+      title='Bit Pricier'
+      results={filterResultsByPrice('$$')}
+      />
+      <ResultsList
+      title='Big Spender'
+      results={filterResultsByPrice('$$$')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
