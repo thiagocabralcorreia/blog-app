de9aa73279ed6eaac506ef5e32fc48debb0a2d4e
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2021-06-13 18:23:32 -0300

    Set up json server

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://ca5af96c82bf.ngrok.io/'
+});
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = ({ navigation }) => {
-  const { state, addBlogPost , deleteBlogPost} = useContext(Context);
+  const { state, deleteBlogPost,getBlogPosts } = useContext(Context);
+
+  useEffect(() => {
+    getBlogPosts();
+
+    const listener = navigation.addListener('didFocus', () => {
+      getBlogPosts();
+    })
+
+    return () => {
+      listener.remove();
+    };
+
+  }, [])
 
   return (
     <View style={styles.container}>
